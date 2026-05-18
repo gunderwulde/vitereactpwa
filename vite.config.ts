f3f0362ac626a7791eb1ce4b16bdf545a57692a5
Fernando Perez <gunderwulde@gmail.com>
2026-05-18 08:17:25 +0200

    Eliminada parte no usada

--- vite.config.ts
+++ vite.config.ts
@@ -8,37 +8,35 @@ export default defineConfig({
   base: '/vitereactpwa/',
   plugins: [
     react(),
-      babel({ presets: [reactCompilerPreset()] }),
-      VitePWA({
-          registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-            includeAssets: ['favicon.svg', 'icons.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
-          manifest: {
-              name: 'Vite React PWA',
-              short_name: 'vitereactpwa',
-              description: 'Vite React Progressive Web Aplication',
-              theme_color: '#ffffff',
-              background_color: '#ffffff',
-              display: 'standalone',
+    babel({ presets: [reactCompilerPreset()] }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true, },
+      includeAssets: ['favicon.svg', 'icons.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'Vite React PWA',
+        short_name: 'vitereactpwa',
+        description: 'Vite React Progressive Web Aplication',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
         start_url: '/vitereactpwa/',
         scope: '/vitereactpwa/',
         icons: [
           {
-              src: 'pwa-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
             purpose: 'any',
           },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any',
-            },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
         ],
-          }
-      })
+      }
+    })
   ],
 })
